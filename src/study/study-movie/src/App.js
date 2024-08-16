cbec6c7c6cfcc8de6cc72152d779bbe503179360
stylish_code <minyoung0101q@gmail.com>
2024-08-16 21:49:19 +0900

    Deps - useEffect의 두 번째 인자

--- src/study/study-movie/src/App.js
+++ src/study/study-movie/src/App.js
@@ -3,19 +3,37 @@ import { useState, useEffect } from "react";
 function App() {
 
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
 
   const onClick = () => {
     setCounter(prev => prev + 1);
   }
+
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  }
+
   console.log("i run all the time");
 
   useEffect(() => {
     console.log("CALL THE API");
   }, [])
 
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword])
+
   return (
     <div>
-      <span>total clics: {counter}</span>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <span>total clicks: {counter}</span>
       <button onClick={onClick}>click me</button>
     </div>
   )
