175cf75710876e161dccef5e8d1c6ecb73d06d94
stylish_code <minyoung0101q@gmail.com>
2024-08-20 22:24:31 +0900

    To Do List part One

--- src/study/study-movie/src/App.js
+++ src/study/study-movie/src/App.js
@@ -1,26 +1,34 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-
-  useEffect(() => {
-    console.log("hi:)");
-    return () => console.log("bye:(");
-  }, [])
-
-  return <h1>Hello</h1>
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]); // 투두 입력값들을 담을 수 있는 array
 
-  const onClick = () => {
-    setShowing(prev => !prev);
+  const onChange = (event) => setToDo(event.target.value);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo(""); // input의 value를 비워주자.
+    setToDos(currentArray => [toDo, ...currentArray]); // toDos 배열에 이전 배열 포함 + 추가
   }
+
+  console.log(toDos); // 잘 작동하는지 확인하기 위해
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+          onChange={onChange}
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   )
 }
