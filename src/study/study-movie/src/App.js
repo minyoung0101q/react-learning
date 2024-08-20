54d24e32ccc7fb36be957f834d0bb36cb878e621
stylish_code <minyoung0101q@gmail.com>
2024-08-21 01:46:52 +0900

    Movie App part One

--- src/study/study-movie/src/App.js
+++ src/study/study-movie/src/App.js
@@ -1,31 +1,41 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=yearv`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
 
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then(response => response.json())
-      .then(json => {
-        setCoins(json); // coins의 state를 바꿔보자.
-        setLoading(false);
-      })
+    getMovies();
   }, [])
-
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ?
-        <strong>Loading...</strong>
+      {loading ? <h1>Loading...</h1>
         :
-        <select>
-          {coins.map(coin => (
-            <option key={coin.id}>
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price}
-            </option>
+        <div>
+          {movies.map(movie => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(g => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       }
     </div>
   )
