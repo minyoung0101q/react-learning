9cc1b3b39c73c2fbcde1ccc903ecb775d6359d56
stylish_code <minyoung0101q@gmail.com>
2024-08-21 02:22:51 +0900

    Movie App part Two

--- src/study/study-movie/src/App.js
+++ src/study/study-movie/src/App.js
@@ -1,43 +1,6 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=yearv`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    getMovies();
-  }, [])
-  console.log(movies);
   return (
-    <div>
-      {loading ? <h1>Loading...</h1>
-        :
-        <div>
-          {movies.map(movie => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map(g => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      }
-    </div>
+    <div></div>
   )
 }
 
