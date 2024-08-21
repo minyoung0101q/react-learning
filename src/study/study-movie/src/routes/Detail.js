57c429f82ae3857f93c0a70e26747ec70c8e1b92
stylish_code <minyoung0101q@gmail.com>
2024-08-21 17:03:15 +0900

    Parameters

--- src/study/study-movie/src/routes/Detail.js
+++ src/study/study-movie/src/routes/Detail.js
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>
+    const { id } = useParams();
+
+    const getMovie = async () => {
+        const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`);
+        const json = await response.json();
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getMovie();
+    }, [])
+    return <h2>Detail</h2>;
 }
 
 export default Detail;
